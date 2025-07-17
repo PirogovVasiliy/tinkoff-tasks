84867af1afd497f00bd4700bf2bb5e6a2d28d9a6
PirogovVasiliy <kamvasilii@gmail.com>
2025-07-17 12:05:16 +0300

    tinkoff 3

--- main.go
+++ main.go
@@ -5,17 +5,29 @@ import (
 )
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	fmt.Println(solve(n))
+	var n, t int
+	fmt.Scan(&n, &t)
+	floors := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&floors[i])
+	}
+	var employee int
+	fmt.Scan(&employee)
+	res := solve(n, t, floors, employee)
+	fmt.Println(res)
+
 }
 
-func solve(n int) int {
-	step := 1
-	count := 0
-	for step < n {
-		step *= 2
-		count++
+func solve(n int, t int, staff []int, emmployee int) int {
+
+	before := staff[emmployee-1] - staff[0]
+	after := staff[n-1] - staff[emmployee-1]
+
+	if before <= t || after <= t {
+		return before + after
+	} else if before < after {
+		return before*2 + after
+	} else {
+		return after*2 + before
 	}
-	return count
 }
